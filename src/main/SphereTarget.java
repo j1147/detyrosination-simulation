378b76950fcb1afdf8edd5a8d97298353db4b4b2
j1147 <195060359+j1147@users.noreply.github.com>
2026-01-17 13:43:37 -0500

    Remove old files

--- src/main/SphereTarget.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class SphereTarget extends Particle {
-	/** Create a spherical target */
-	public SphereTarget(double[] vector, double radius) {
-		// This runs the constructor in the Particle file. It only initializes the value and is done to save code.
-		super(vector, radius);
-	}
-}
